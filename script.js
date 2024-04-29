b65b8da9a248a4752d4aaabfa6c2fab468dea711
Piyush Sharma <146875804+piyush12452@users.noreply.github.com>
2024-04-29 17:44:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function textAnimation(){
+    var typed= new Typed("#span-text",{
+        strings:["Frontend Developer" , "Web Developer" , "Python Developer"],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+    })
+}
+
+textAnimation();
